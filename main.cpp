467df0d69c7f8890313b8347530ff3ce78c8de19
jonathanykim <jkim1087@ucr.edu>
2020-08-20 12:21:02 -0700

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "menu.hpp"
+
+
+using namespace std;
+
+int main() {
+    Menu calendar = new Menu();
+    calendar.PrintMenu();
+
+
+    return 0;
+}
